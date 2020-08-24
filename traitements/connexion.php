a36c0f5a99cd8a1aada751fbdfb034bcfb6d9e76
Dimitri ONGOUA <ongouadimitri5@gmail.com>
2020-08-24 14:14:10 +0100

    Vérification du token avec base de données

--- traitements/connexion.php
+++ traitements/connexion.php
@@ -3,6 +3,9 @@
 // dans ce script
 session_start();
 
+// Importation du fichier de gestion de la base de données
+require_once '../utilitaires/donnees.php';
+
 // Vérification de l'existence du jeton
 if (!isset($_POST["_jeton_"]) or empty($_POST["_jeton_"])) {
     redirection("../pages/connexion.php");
@@ -11,11 +14,22 @@ if (!isset($_POST["_jeton_"]) or empty($_POST["_jeton_"])) {
 // Récupération du jeton
 $jeton = $_POST["_jeton_"];
 
+// Vérification du jeton
+$params = array(
+    "collection" => "comptes",
+    "critere" => ["jeton" => $jeton]
+);
+
+$bdd = new Donnees();
+$res = $bdd->get($params);
+
+if (!$res) {
+    $_SESSION["message"] = 'Aucun compte n\'est associé à ce jeton';
+    redirection("../pages/connexion.php");
+}
+
 // Enregistrement du jeton dans la session
 $_SESSION["_jeton_"] = $jeton;
-
-// 
-
 
 // On redirige vers le panel
 redirection("../pages/panel.php");
@@ -26,6 +40,3 @@ function redirection($destination)
     header("Location: $destination");
     exit();
 }
-
-
-?>
--- utilitaires/donnees.php
+++ utilitaires/donnees.php
@@ -1,23 +1,55 @@
 <?php
+require_once 'debug.php';
 
 class Donnees
 {
 
     private $configFile;
+    private $baseUrl = "https://carte-mere.netlify.app/.netlify/functions";
 
-    public static function config($configFile)
+    public function get(Array $params)
     {
-        if (file_exists($configFile)) {
-            $donnees = new Donnees();
-            $donnees->setConfig($configFile);
-            return $donnees;
-        } else {
-            die("Le fichier de configuration est introuvable.");
-        }
+        $url = $this->baseUrl . "/get";
+
+        $docs = $this->requete($url, $params);
+
+        //header('Content-Type: application/json');
+        $docs = json_decode($docs, true);
+
+        return $docs;
     }
 
-    public function setConfig(String $configFile)
+    /**
+     * Send an request to devices
+     * @param String $url Endpoint for database function handler
+     * @param String $fields Fields to send
+     */
+    function requete(String $url, Array $fields)
     {
-        $this->configFile = $configFile;
+        $fields = json_encode($fields);
+
+        $headers = array(
+            'Content-Type: application/json'
+        );
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+
+        $resultat = curl_exec($ch);
+
+        /*$status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        if ($status != 200) {
+        	die("Error: call to URL $url failed with status $status, response $resultat, curl_error " . curl_error($ch) . ", curl_errno " . curl_errno($ch));
+        }*/
+
+        curl_close($ch);
+
+        return $resultat;
     }
 }
